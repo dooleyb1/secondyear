9e095d6ef786307f420f627408dfa97d2ca11fcb
Brandon Dooley <b.d.1997@hotmail.com>
2017-10-18 19:19:40 +0100

    Algorithms assignment 3, lectures

---
+++ MT/CS2010_Algorithms/Assignments/assignment2/DoublyLinkedList.java
@@ -0,0 +1,272 @@
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+// -------------------------------------------------------------------------
+/**
+ *  This class contains the methods of Doubly Linked List.
+ *
+ *  @author  
+ *  @version 13/10/16 18:15
+ */
+
+
+/**
+ * Class DoublyLinkedList: implements a *generic* Doubly Linked List.
+ * @param <T> This is a type parameter. T is used as a class name in the
+ * definition of this class.
+ *
+ * When creating a new DoublyLinkedList, T should be instantiated with an
+ * actual class name that extends the class Comparable.
+ * Such classes include String and Integer.
+ *
+ * For example to create a new DoublyLinkedList class containing String data: 
+ *    DoublyLinkedList<String> myStringList = new DoublyLinkedList<String>();
+ *
+ * The class offers a toString() method which returns a comma-separated sting of
+ * all elements in the data structure.
+ * 
+ * This is a bare minimum class you would need to completely implement.
+ * You can add additional methods to support your code. Each method will need
+ * to be tested by your jUnit tests -- for simplicity in jUnit testing
+ * introduce only public methods.
+ */
+class DoublyLinkedList<T extends Comparable<T>>
+{
+
+    /**
+     * private class DLLNode: implements a *generic* Doubly Linked List node.
+     */
+    private class DLLNode
+    {
+        public final T data; // this field should never be updated. It gets its
+                             // value once from the constructor DLLNode.
+        public DLLNode next;
+        public DLLNode prev;
+    
+        /**
+         * Constructor
+         * @param theData : data of type T, to be stored in the node
+         * @param prevNode : the previous Node in the Doubly Linked List
+         * @param nextNode : the next Node in the Doubly Linked List
+         * @return DLLNode
+         */
+        public DLLNode(T theData, DLLNode prevNode, DLLNode nextNode) 
+        {
+          data = theData;
+          prev = prevNode;
+          next = nextNode;
+        }
+    }
+
+    // Fields head and tail point to the first and last nodes of the list.
+    private DLLNode head, tail;
+
+    /**
+     * Constructor
+     * @return DoublyLinkedList
+     */
+    public DoublyLinkedList() 
+    {
+      head = null;
+      tail = null;
+    }
+
+    /**
+     * Tests if the doubly linked list is empty
+     * @return true if list is empty, and false otherwise
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public boolean isEmpty()
+    {
+      // TODO
+      return true;
+    }
+
+    /**
+     * Inserts an element in the doubly linked list
+     * @param pos : The integer location at which the new data should be
+     *      inserted in the list. We assume that the first position in the list
+     *      is 0 (zero). If pos is less than 0 then add to the head of the list.
+     *      If pos is greater or equal to the size of the list then add the
+     *      element at the end of the list.
+     * @param data : The new data of class T that needs to be added to the list
+     * @return none
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public void insertBefore( int pos, T data ) 
+    {
+      //TODO
+      return;
+    }
+
+    /**
+     * Returns the data stored at a particular position
+     * @param pos : the position
+     * @return the data at pos, if pos is within the bounds of the list, and null otherwise.
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     *
+     * Worst-case precise runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public T get(int pos) 
+    {
+      //TODO
+      return null;
+    }
+
+    /**
+     * Deletes the element of the list at position pos.
+     * First element in the list has position 0. If pos points outside the
+     * elements of the list then no modification happens to the list.
+     * @param pos : the position to delete in the list.
+     * @return true : on successful deletion, false : list has not been modified. 
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public boolean deleteAt(int pos) 
+    {
+      //TODO
+      return false;
+    }
+
+    /**
+     * Reverses the list.
+     * If the list contains "A", "B", "C", "D" before the method is called
+     * Then it should contain "D", "C", "B", "A" after it returns.
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public void reverse()
+    {
+      //TODO
+    }
+
+
+    /*----------------------- STACK */
+    /**
+     * This method should behave like the usual push method of a Stack ADT.
+     * If only the push and pop methods are called the data structure should behave like a stack.
+     * How exactly this will be represented in the Doubly Linked List is up to the programmer.
+     * @param item : the item to push on the stack
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public void push(T item) 
+    {
+      //TODO
+    }
+
+    /**
+     * This method should behave like the usual pop method of a Stack ADT.
+     * If only the push and pop methods are called the data structure should behave like a stack.
+     * How exactly this will be represented in the Doubly Linked List is up to the programmer.
+     * @return the last item inserted with a push; or null when the list is empty.
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public T pop() 
+    {
+      //TODO
+      return null;
+    }
+
+    /*----------------------- QUEUE */
+ 
+    /**
+     * This method should behave like the usual enqueue method of a Queue ADT.
+     * If only the enqueue and dequeue methods are called the data structure should behave like a FIFO queue.
+     * How exactly this will be represented in the Doubly Linked List is up to the programmer.
+     * @param item : the item to be enqueued to the stack
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public void enqueue(T item) 
+    {
+      //TODO
+    }
+
+     /**
+     * This method should behave like the usual dequeue method of a Queue ADT.
+     * If only the enqueue and dequeue methods are called the data structure should behave like a FIFO queue.
+     * How exactly this will be represented in the Doubly Linked List is up to the programmer.
+     * @return the earliest item inserted with a push; or null when the list is empty.
+     *
+     * Worst-case asymptotic runtime cost: TODO
+     *
+     * Justification:
+     *  TODO
+     */
+    public T dequeue() 
+    {
+      //TODO
+      return null;
+    }
+ 
+
+    /**
+     * @return a string with the elements of the list as a comma-separated
+     * list, from beginning to end
+     *
+     * Worst-case asymptotic runtime cost:   Theta(n)
+     *
+     * Justification:
+     *  We know from the Java documentation that StringBuilder's append() method runs in Theta(1) asymptotic time.
+     *  We assume all other method calls here (e.g., the iterator methods above, and the toString method) will execute in Theta(1) time.
+     *  Thus, every one iteration of the for-loop will have cost Theta(1).
+     *  Suppose the doubly-linked list has 'n' elements.
+     *  The for-loop will always iterate over all n elements of the list, and therefore the total cost of this method will be n*Theta(1) = Theta(n).
+     */
+    public String toString() 
+    {
+      StringBuilder s = new StringBuilder();
+      boolean isFirst = true; 
+
+      // iterate over the list, starting from the head
+      for (DLLNode iter = head; iter != null; iter = iter.next)
+      {
+        if (!isFirst)
+        {
+          s.append(",");
+        } else {
+          isFirst = false;
+        }
+        s.append(iter.data.toString());
+      }
+
+      return s.toString();
+    }
+
+
+}
+
+
---
+++ MT/CS2010_Algorithms/Assignments/assignment2/DoublyLinkedListTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//-------------------------------------------------------------------------
+/**
+ *  Test class for Doubly Linked List
+ *
+ *  @author  
+ *  @version 13/10/16 18:15
+ */
+@RunWith(JUnit4.class)
+public class DoublyLinkedListTest
+{
+    //~ Constructor ........................................................
+    @Test
+    public void testConstructor()
+    {
+      new DoublyLinkedList<Integer>();
+    }
+
+    //~ Public Methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Check if the insertBefore works
+     */
+    @Test
+    public void testInsertBefore()
+    {
+        // test non-empty list
+        DoublyLinkedList<Integer> testDLL = new DoublyLinkedList<Integer>();
+        testDLL.insertBefore(0,1);
+        testDLL.insertBefore(1,2);
+        testDLL.insertBefore(2,3);
+
+        testDLL.insertBefore(0,4);
+        assertEquals( "Checking insertBefore to a list containing 3 elements at position 0", "4,1,2,3", testDLL.toString() );
+        testDLL.insertBefore(1,5);
+        assertEquals( "Checking insertBefore to a list containing 4 elements at position 1", "4,5,1,2,3", testDLL.toString() );
+        testDLL.insertBefore(2,6);       
+        assertEquals( "Checking insertBefore to a list containing 5 elements at position 2", "4,5,6,1,2,3", testDLL.toString() );
+        testDLL.insertBefore(-1,7);        
+        assertEquals( "Checking insertBefore to a list containing 6 elements at position -1 - expected the element at the head of the list", "7,4,5,6,1,2,3", testDLL.toString() );
+        testDLL.insertBefore(7,8);        
+        assertEquals( "Checking insertBefore to a list containing 7 elemenets at position 8 - expected the element at the tail of the list", "7,4,5,6,1,2,3,8", testDLL.toString() );
+        testDLL.insertBefore(700,9);        
+        assertEquals( "Checking insertBefore to a list containing 8 elements at position 700 - expected the element at the tail of the list", "7,4,5,6,1,2,3,8,9", testDLL.toString() );
+
+        // test empty list
+        testDLL = new DoublyLinkedList<Integer>();
+        testDLL.insertBefore(0,1);        
+        assertEquals( "Checking insertBefore to an empty list at position 0 - expected the element at the head of the list", "1", testDLL.toString() );
+        testDLL = new DoublyLinkedList<Integer>();
+        testDLL.insertBefore(10,1);        
+        assertEquals( "Checking insertBefore to an empty list at position 10 - expected the element at the head of the list", "1", testDLL.toString() );
+        testDLL = new DoublyLinkedList<Integer>();
+        testDLL.insertBefore(-10,1);        
+        assertEquals( "Checking insertBefore to an empty list at position -10 - expected the element at the head of the list", "1", testDLL.toString() );
+     }
+
+    // TODO: add more tests here. Each line of code in DoublyLinkedList.java should
+    // be executed at least once from at least one test.
+
+}
