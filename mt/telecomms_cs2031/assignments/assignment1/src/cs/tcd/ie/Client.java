244120db7c42bbfcef19e0f689b32c443de470d4
Brandon <dooleyb1@tcd.ie>
2017-11-09 13:01:18 +0000

    timeout method added

--- mt/telecomms_cs2031/assignments/assignment1/src/cs/tcd/ie/Client.java
+++ mt/telecomms_cs2031/assignments/assignment1/src/cs/tcd/ie/Client.java
@@ -23,6 +23,7 @@ import tcdIO.*;
 public class Client extends Node {
 	static final int DEFAULT_DST_PORT = 50000;
 	static final int DEFAULT_GATEWAY_PORT = 40000;
+	static final int TIMER_VALUE = 10000;
 	static final String DEFAULT_DST_NODE = "localhost";	
 	byte[] seqNumber;
 	DatagramPacket previousPacket;
@@ -48,6 +49,8 @@ public class Client extends Node {
 			this.sourcePortNumber = srcPort;
 			//Creates socket at srcPort
 			socket= new DatagramSocket(sourcePortNumber);
+			//Sets timer mechanism
+			socket.setSoTimeout(TIMER_VALUE);
 			this.seqNumber = ByteBuffer.allocate(4).putInt(0).array();
 			this.flag = ByteBuffer.allocate(4).putInt(0).array();
 			previousPacket = null;
@@ -84,43 +87,49 @@ public class Client extends Node {
 	 * 
 	 * @throws Exception
 	 */
-	public synchronized void carryOn(DatagramPacket packet) throws IOException, InterruptedException{
-		byte[] newPayload = null;
-		byte[] newBuffer = null;
-		byte[] newFlag = new byte[PacketContent.FLAG_LENGTH];
-		byte[] oldBuffer = null;
-		byte[] headerData = new byte[PacketContent.HEADER_LENGTH];
-		
-		newPayload = (terminal.readString("New string to send: ")).getBytes();
-		oldBuffer = packet.getData();
-		//Reset flag
-		newFlag = ByteBuffer.allocate(4).putInt(0).array();
-		
-		//Update sequence number
-		int sequenceNumber = ByteBuffer.wrap(this.seqNumber).getInt();
-		sequenceNumber++;
-		this.seqNumber = ByteBuffer.allocate(4).putInt(sequenceNumber).array();
-		
-		//Creates a buffer to contain the information
-		newBuffer= new byte[PacketContent.HEADER_LENGTH + newPayload.length];
-		//Transfer old header data from old buffer
-		System.arraycopy(oldBuffer, 0, headerData, 0, PacketContent.HEADER_LENGTH);
-		System.arraycopy(headerData, 0, newBuffer, 0 , PacketContent.HEADER_LENGTH);
-		//Transfer in new payload
-		System.arraycopy(newPayload, 0, newBuffer, PacketContent.HEADER_LENGTH, newPayload.length);
-		
-		//Tranfer in updated flag
-		System.arraycopy(newFlag, 0, newBuffer, 
-				(PacketContent.HEADER_LENGTH-PacketContent.FLAG_LENGTH-PacketContent.RESPONSE_FLAG_LENGTH-PacketContent.RESPONSE_NUMBER_LENGTH),
-				PacketContent.FLAG_LENGTH);
-		
-		//Transfer in new sequence number
-		System.arraycopy(this.seqNumber, 0, newBuffer, (PacketContent.DST_ADDRESS_LENGTH + PacketContent.SRC_ADDRESS_LENGTH), PacketContent.SEQ_NUMBER_LENGTH);
-		
-		terminal.println("\nSending new packet to gateway...");
-		packet= new DatagramPacket(newBuffer, newBuffer.length, gatewayAddress);
-		socket.send(packet);
-		terminal.println("New packet sent to gateway\n");
+	public synchronized void carryOn(DatagramPacket packet) throws IOException, InterruptedException, SocketTimeoutException{
+		try {
+			byte[] newPayload = null;
+			byte[] newBuffer = null;
+			byte[] newFlag = new byte[PacketContent.FLAG_LENGTH];
+			byte[] oldBuffer = null;
+			byte[] headerData = new byte[PacketContent.HEADER_LENGTH];
+			
+			newPayload = (terminal.readString("New string to send: ")).getBytes();
+			oldBuffer = packet.getData();
+			//Reset flag
+			newFlag = ByteBuffer.allocate(4).putInt(0).array();
+			
+			//Update sequence number
+			int sequenceNumber = ByteBuffer.wrap(this.seqNumber).getInt();
+			sequenceNumber++;
+			this.seqNumber = ByteBuffer.allocate(4).putInt(sequenceNumber).array();
+			
+			//Creates a buffer to contain the information
+			newBuffer= new byte[PacketContent.HEADER_LENGTH + newPayload.length];
+			//Transfer old header data from old buffer
+			System.arraycopy(oldBuffer, 0, headerData, 0, PacketContent.HEADER_LENGTH);
+			System.arraycopy(headerData, 0, newBuffer, 0 , PacketContent.HEADER_LENGTH);
+			//Transfer in new payload
+			System.arraycopy(newPayload, 0, newBuffer, PacketContent.HEADER_LENGTH, newPayload.length);
+			
+			//Tranfer in updated flag
+			System.arraycopy(newFlag, 0, newBuffer, 
+					(PacketContent.HEADER_LENGTH-PacketContent.FLAG_LENGTH-PacketContent.RESPONSE_FLAG_LENGTH-PacketContent.RESPONSE_NUMBER_LENGTH),
+					PacketContent.FLAG_LENGTH);
+			
+			//Transfer in new sequence number
+			System.arraycopy(this.seqNumber, 0, newBuffer, (PacketContent.DST_ADDRESS_LENGTH + PacketContent.SRC_ADDRESS_LENGTH), PacketContent.SEQ_NUMBER_LENGTH);
+			
+			terminal.println("\nSending new packet to gateway...");
+			packet= new DatagramPacket(newBuffer, newBuffer.length, gatewayAddress);
+			socket.send(packet);
+			terminal.println("New packet sent to gateway\n");
+		}
+		catch (SocketTimeoutException s) {
+			terminal.println("Socket time-out. Re-sending packet...");
+	        resendPacket(packet);
+	      }
 	}
 	
 	/**
@@ -130,30 +139,35 @@ public class Client extends Node {
 	 * 
 	 * @throws Exception
 	 */
-	public synchronized void resendPacket(DatagramPacket packet) throws IOException, InterruptedException{
-		
-		byte[] newFlag = new byte[PacketContent.FLAG_LENGTH];
-		byte[] newResponseFlag = new byte[PacketContent.RESPONSE_FLAG_LENGTH];
-		byte[] oldBuffer = null;
-		
-		terminal.println("Generating packet to re-send...");
-		oldBuffer = packet.getData();
-		//Reset flag
-		newFlag = ByteBuffer.allocate(4).putInt(0).array();
-		//Reset response flag
-		newFlag = ByteBuffer.allocate(4).putInt(1).array();
-		
-		//Transfer new flag to header data from old buffer
-		System.arraycopy(newFlag, 0, oldBuffer, PacketContent.HEADER_LENGTH-(PacketContent.FLAG_LENGTH + PacketContent.RESPONSE_FLAG_LENGTH+
-				PacketContent.RESPONSE_NUMBER_LENGTH), PacketContent.FLAG_LENGTH);
-		//Transfer new responeFlag to header data from old buffer
-		System.arraycopy(newResponseFlag, 0, oldBuffer, PacketContent.HEADER_LENGTH-(PacketContent.RESPONSE_FLAG_LENGTH+ PacketContent.RESPONSE_NUMBER_LENGTH)
-		, PacketContent.RESPONSE_FLAG_LENGTH);
-		
-		terminal.println("\nRe-sending packet to gateway...");
-		packet= new DatagramPacket(oldBuffer, oldBuffer.length, gatewayAddress);
-		socket.send(packet);
-		terminal.println("Packet re-sent to gateway\n");
+	public synchronized void resendPacket(DatagramPacket packet) throws IOException, InterruptedException, SocketTimeoutException{
+		try {
+			byte[] newFlag = new byte[PacketContent.FLAG_LENGTH];
+			byte[] newResponseFlag = new byte[PacketContent.RESPONSE_FLAG_LENGTH];
+			byte[] oldBuffer = null;
+			
+			terminal.println("Generating packet to re-send...");
+			oldBuffer = packet.getData();
+			//Reset flag
+			newFlag = ByteBuffer.allocate(4).putInt(0).array();
+			//Reset response flag
+			newFlag = ByteBuffer.allocate(4).putInt(1).array();
+			
+			//Transfer new flag to header data from old buffer
+			System.arraycopy(newFlag, 0, oldBuffer, PacketContent.HEADER_LENGTH-(PacketContent.FLAG_LENGTH + PacketContent.RESPONSE_FLAG_LENGTH+
+					PacketContent.RESPONSE_NUMBER_LENGTH), PacketContent.FLAG_LENGTH);
+			//Transfer new responeFlag to header data from old buffer
+			System.arraycopy(newResponseFlag, 0, oldBuffer, PacketContent.HEADER_LENGTH-(PacketContent.RESPONSE_FLAG_LENGTH+ PacketContent.RESPONSE_NUMBER_LENGTH)
+			, PacketContent.RESPONSE_FLAG_LENGTH);
+			
+			terminal.println("\nRe-sending packet to gateway...");
+			packet= new DatagramPacket(oldBuffer, oldBuffer.length, gatewayAddress);
+			socket.send(packet);
+			terminal.println("Packet re-sent to gateway\n");
+		}
+		catch (SocketTimeoutException s) {
+			terminal.println("Socket time-out. Re-sending packet...");
+	        resendPacket(packet);
+	      }
 	}
 	/**
 	 * Sender Method
@@ -162,25 +176,26 @@ public class Client extends Node {
 	/**
 	 * @throws Exception
 	 */
-	public synchronized void start() throws Exception {
-		DatagramPacket packet= null;
-
-		byte[] payload= null;
-		byte[] dstAddress = null;
-		byte[] srcAddress = null;
-		byte[] sequenceNum = null;
-		byte[] flag = null;
-		byte[] responseFlag = null;
-		byte[] responseNumber = null;
-		byte[] buffer= null;
-		
+	public synchronized void start() throws Exception, SocketTimeoutException {
+		try{
+			DatagramPacket packet= null;
+	
+			byte[] payload= null;
+			byte[] dstAddress = null;
+			byte[] srcAddress = null;
+			byte[] sequenceNum = null;
+			byte[] flag = null;
+			byte[] responseFlag = null;
+			byte[] responseNumber = null;
+			byte[] buffer= null;
+			
 			dstAddress = new byte[PacketContent.DST_ADDRESS_LENGTH];
 			srcAddress = new byte[PacketContent.SRC_ADDRESS_LENGTH];
 			responseFlag = new byte[PacketContent.RESPONSE_FLAG_LENGTH];
 			responseNumber = new byte[PacketContent.RESPONSE_NUMBER_LENGTH];
 			sequenceNum = this.seqNumber;
 			flag = this.flag;
-		
+			
 			//Reads and sorts the relevant information into byte arrays
 			int dst = terminal.readInt("Destination address: ");
 			payload = (terminal.readString("String to send: ")).getBytes();
@@ -191,28 +206,32 @@ public class Client extends Node {
 			responseNumber = ByteBuffer.allocate(PacketContent.RESPONSE_NUMBER_LENGTH).putInt(0).array();
 			System.out.print(ByteBuffer.wrap(dstAddress).getInt());
 			srcAddress = ByteBuffer.allocate(PacketContent.SRC_ADDRESS_LENGTH).putInt(this.sourcePortNumber).array();
-			
+				
 			//Creates a buffer to contain the information
 			buffer= new byte[PacketContent.HEADER_LENGTH + payload.length];
-			
+				
 			//Encloses the above information into a buffer containing an array of bytes
 			System.arraycopy(dstAddress, 0, buffer, 0, dstAddress.length);
-			System.arraycopy(srcAddress, 0, buffer, dstAddress.length, srcAddress.length);
+			System.arraycopy(srcAddress, 0, buffer, dstAddress.length, srcAddress.length);	
 			System.arraycopy(sequenceNum, 0, buffer, (dstAddress.length+srcAddress.length), PacketContent.SEQ_NUMBER_LENGTH);
 			System.arraycopy(flag, 0, buffer, (dstAddress.length+srcAddress.length+PacketContent.SEQ_NUMBER_LENGTH), PacketContent.FLAG_LENGTH);
 			System.arraycopy(responseFlag, 0, buffer, (PacketContent.HEADER_LENGTH-(PacketContent.RESPONSE_FLAG_LENGTH+PacketContent.RESPONSE_NUMBER_LENGTH)),
-					PacketContent.RESPONSE_FLAG_LENGTH);
+						PacketContent.RESPONSE_FLAG_LENGTH);
 			System.arraycopy(responseNumber, 0, buffer, PacketContent.HEADER_LENGTH-PacketContent.RESPONSE_NUMBER_LENGTH, PacketContent.RESPONSE_NUMBER_LENGTH);
 			System.arraycopy(payload, 0, buffer, PacketContent.HEADER_LENGTH, payload.length);
-			
+				
 			terminal.println("\nSending packet to gateway...");
 			packet= new DatagramPacket(buffer, buffer.length, gatewayAddress);
 			this.previousPacket = packet;
 			socket.send(packet);
 			terminal.println("Packet sent to gateway\n");
-			
-			
+				
+				
 			this.wait();
+		}catch (SocketTimeoutException s) {
+			terminal.println("Socket time-out. Re-sending packet...");
+	        resendPacket(this.previousPacket);
+	      }		
 	}
 	
 	/**
