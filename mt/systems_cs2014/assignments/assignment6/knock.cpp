7cba1771badc21f7d3055f53b6f4f205f45cdc7e
Brandon <dooleyb1@tcd.ie>
2017-11-29 13:50:52 +0000

    working between local client and server

--- mt/systems_cs2014/assignments/assignment6/knock.cpp
+++ mt/systems_cs2014/assignments/assignment6/knock.cpp
@@ -1,14 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <strings.h>
 #include <unistd.h>
-#include <iostream>
 #include <string>
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
+
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+
 using namespace std;
-using boost::asio::ip::tcp;
 
 
 /* Globals */
@@ -27,6 +34,10 @@ int main(int argc, char **argv)
 	//Initialise flags to false
 	hostflag = portflag = helpflag = webflag = fileflag = false;
 
+	int port;
+	string host;
+	char getrequest[1000], message[1000], server_reply[2000];
+
 	int n,i,j;
 	//Process command line args
 	for(n=i=1;i<argc;i=n) {
@@ -37,11 +48,15 @@ int main(int argc, char **argv)
 			switch(argv[i][j]) {
 			case 'h':
 			  hostflag = true;
+			  host = argv[2];
  			  cout << "hostflag set to true" << "\n";
+			  cout << "hostvalue entered = " << host << "\n";
 			  break;
 			case 'p':
 			  portflag = true;
+			  port = atoi(argv[4]);
 			  cout << "portflag set to true" << "\n";
+			  cout << "portvalue entered = " << port << "\n";
 			  break;
 			case 'H':
 			  helpflag = true;
@@ -49,7 +64,9 @@ int main(int argc, char **argv)
 			  break;
 			case 'w':
 			  webflag = true;
+ 			  memcpy(getrequest, argv[6], strlen(argv[6])+1);
 			  cout << "webflag set to true" << "\n";
+ 			  cout << "HTTP get request is : " << getrequest << "\n";
 			  break;
 			case 'f':
 			  fileflag = true;
@@ -75,22 +92,29 @@ int main(int argc, char **argv)
 			for(j=0;argv[i][j];j++) {
 	    		if (!strcmp("host", argv[i])) {
 	     		  hostflag = true;
-			      cout << "hostflag set to true" << "\n";
+			  host = argv[2];
+ 			  cout << "hostflag set to true" << "\n";
+			  cout << "hostvalue entered = " << host << "\n";
 	     		  break;
 	    		}
 	    		if (!strcmp("port",argv[i])) {
 	    		  portflag = true;
-				  cout << "portflag set to true" << "\n";
+			  port = atoi(argv[4]);
+			  cout << "portflag set to true" << "\n";
+			  cout << "portvalue entered = " << port << "\n";
 	     		  break;
 	    		}
 				if (!strcmp("help",argv[i])) {
 	    		  helpflag = true;
-				  cout << "helpflag set to true" << "\n";
+			  cout << "helpflag set to true" << "\n";
 	     		  break;
 	    		}
 				if (!strcmp("web",argv[i])) {
 	    		  webflag = true;
-				  cout << "webflag set to true" << "\n";
+			  cout << "webflag set to true" << "\n";
+			  memcpy(getrequest, argv[6], strlen(argv[6])+1);
+			  cout << "webflag set to true" << "\n";
+ 			  cout << "HTTP get request is : " << getrequest << "\n";
 	     		  break;
 	    		}
 				if (!strcmp("file",argv[i])) {
@@ -114,61 +138,66 @@ int main(int argc, char **argv)
 	}
 	
 	else{
+		// use DNS to get IP address
+    		struct hostent *hostEntry;
+    		hostEntry = gethostbyname(host.c_str());
 
- 		try
-  		{
-			// Any program that uses asio need to have at least one io_service object
-			boost::asio::io_service io_service;
+    		if (!hostEntry) {
+        		cout << "No such host name: " << host << endl;
+        		exit(-1);
+		}
+		else
+			cout << "Successfly found hostname..." << "\n";	
+		
+		// setup socket address structure
+    		struct sockaddr_in server_addr;
+   		memset(&server_addr,0,sizeof(server_addr));
+    		server_addr.sin_family = AF_INET;
+    		server_addr.sin_port = htons(port);
+    		memcpy(&server_addr.sin_addr, hostEntry->h_addr_list[0], hostEntry->h_length);
 
-			// Convert the server name that was specified as a parameter to the application, to a TCP endpoint. 
-			// To do this, we use an ip::tcp::resolver object.
-			tcp::resolver resolver(io_service);
+      		// create socket
+    		int server = socket(PF_INET,SOCK_STREAM,0);
+    		if (server < 0) {
+        		perror("socket");
+        		exit(-1);
+    		}
+		else
+			cout << "Socket successfully created..." << "\n";
 
-			// A resolver takes a query object and turns it into a list of endpoints. 
-			// We construct a query using the name of the server, specified in argv[1], 
-			// and the name of the service, in this case "daytime".
-			tcp::resolver::query query(argv[2], "daytime");
+      		// connect to server
+    		if (connect(server,(const struct sockaddr *)&server_addr,sizeof(server_addr)) < 0) {
+       			perror("connect");
+        		exit(-1);
+    		}
+		else
+			cout << "Successfly connected to server..." << "\n";	
 
-			// The list of endpoints is returned using an iterator of type ip::tcp::resolver::iterator. 
-			// A default constructed ip::tcp::resolver::iterator object can be used as an end iterator.
-			tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-			// Now we create and connect the socket.
-			// The list of endpoints obtained above may contain both IPv4 and IPv6 endpoints, 
-			// so we need to try each of them until we find one that works. 
-			// This keeps the client program independent of a specific IP version. 
-			// The boost::asio::connect() function does this for us automatically.
-			tcp::socket socket(io_service);
-			boost::asio::connect(socket, endpoint_iterator);
-
-			// The connection is open. All we need to do now is read the response from the daytime service.
-			for (;;)
+		//keep communicating with server
+	      	while(1)
+	   	{
+			printf("Enter message : ");
+			scanf("%s" , message);
+		 
+			//Send some data
+			if( send(server , message , strlen(message) , 0) < 0)
 			{
-			  // We use a boost::array to hold the received data. 
-			  boost::array<char, 128> buf;
-			  boost::system::error_code error;
-
-			  // The boost::asio::buffer() function automatically determines 
-			  // the size of the array to help prevent buffer overruns.
-			  size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-			  // When the server closes the connection, 
-			  // the ip::tcp::socket::read_some() function will exit with the boost::asio::error::eof error, 
-			  // which is how we know to exit the loop.
-			  if (error == boost::asio::error::eof)
-				break; // Connection closed cleanly by peer.
-			  else if (error)
-				throw boost::system::system_error(error); // Some other error.
-
-			  std::cout.write(buf.data(), len);
+		    		puts("Send failed");
+		    		return 1;
 			}
-		  }
-		  // handle any exceptions that may have been thrown.
-		  catch (std::exception& e)
-		  {
-			std::cerr << e.what() << std::endl;
-		  }
-
-		  return 0;
+		 
+			//Receive a reply from the server
+			if( recv(server , server_reply , 2000 , 0) < 0)
+			{
+		    		puts("recv failed");
+		    		break;
+			}
+		 
+			puts("Server reply :");
+			puts(server_reply);
+	    	}
+	     
+	   	close(server);
+	    	return 0;  			
 	}
 }
--- mt/systems_cs2014/assignments/assignment6/server.cpp
+++ mt/systems_cs2014/assignments/assignment6/server.cpp
@@ -1,47 +1,73 @@
-#include <ctime>
-#include <iostream>
-#include <string>
-#include <boost/asio.hpp>
-
-using boost::asio::ip::tcp;
-
-std::string make_daytime_string()
+/*
+    C socket server example
+*/
+ 
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+ 
+int main(int argc , char *argv[])
 {
-  std::time_t now = std::time(0);
-  return std::ctime(&now);
-}
-
-int main()
-{
-  try
-  {
-    // Any program that uses asio need to have at least one io_service object
-    boost::asio::io_service io_service;
-
-    // acceptor object needs to be created to listen for new connections
-    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
-
-    for (;;)
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
     {
-      // creates a socket
-      tcp::socket socket(io_service);
-
-      // wait and listen
-      acceptor.accept(socket);
-
-      // prepare message to send back to client
-      std::string message = make_daytime_string();
-
-      boost::system::error_code ignored_error;
-
-      // writing the message for current time
-      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+        printf("Could not create socket");
     }
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
-
-  return 0;
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+     
+    //Receive a message from client
+    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Send the message back to client
+        write(client_sock , client_message , strlen(client_message));
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+     
+    return 0;
 }
