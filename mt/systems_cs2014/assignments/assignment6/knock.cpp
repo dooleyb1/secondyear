dd203de633adb1296d6c375f53fd5ad9cbe7692f
Brandon Dooley <dooleyb1@tcd.ie>
2017-12-02 11:41:52 +0000

    recieving data well

--- mt/systems_cs2014/assignments/assignment6/knock.cpp
+++ mt/systems_cs2014/assignments/assignment6/knock.cpp
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <string>
-
-
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netdb.h>
@@ -13,15 +11,21 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include<fcntl.h> 		
 #include <fstream>
+#include <sys/time.h>
 #include <iostream>
 
 using namespace std;
+#define BUFSIZE 4096
 
 
 /* Globals */
 bool hostflag, portflag, helpflag, webflag, fileflag;
+//Receiving function
+static int readSocket(int socket, char *buffer);
 
 void usage(int n)
 {
@@ -36,12 +40,15 @@ int main(int argc, char **argv)
 	//Initialise flags to false
 	hostflag = portflag = helpflag = webflag = fileflag = false;
 
-	char host[500];
-    char request[500];
-    char merged_request[1000];
+    char host[500];
+    char resource_path[500];
+    char request[1000];
+    char* input_buffer;
+    input_buffer = (char*) malloc(20000);
     struct hostent *server;
     struct sockaddr_in serveraddr;
     int port;
+    int result;
 
 	int n,i,j;
 	//Process command line args
@@ -69,9 +76,9 @@ int main(int argc, char **argv)
 			  break;
 			case 'w':
 			  webflag = true;
-			  memcpy(request, argv[6], strlen(argv[6])+1);
+			  memcpy(resource_path, argv[6], strlen(argv[6])+1);
 			  cout << "webflag set to true" << "\n";
-			  cout << "HTTP get request is : " << request << "\n";	
+			  cout << "HTTP get request is : " << resource_path << "\n";	
 			  break;
 			case 'f':
 			  fileflag = true;
@@ -116,9 +123,9 @@ int main(int argc, char **argv)
 	    		}
 				if (!strcmp("web",argv[i])) {
 	    		 	 webflag = true;
-			 		 memcpy(request, argv[6], strlen(argv[6])+1);
+			 		 memcpy(resource_path, argv[6], strlen(argv[6])+1);
 			  		 cout << "webflag set to true" << "\n";
-			  		 cout << "HTTP get request is : " << request << "\n";
+			  		 cout << "HTTP get request is : " << resource_path << "\n";
 	     		     break;
 	    		}
 				if (!strcmp("file",argv[i])) {
@@ -166,39 +173,57 @@ int main(int argc, char **argv)
         	}
 	}
 	printf("\n");
- 
-    	bzero((char *) &serveraddr, sizeof(serveraddr));
-    	serveraddr.sin_family = AF_INET;
- 
-    	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-     
-    	serveraddr.sin_port = htons(port);
-     
-    	if (connect(tcpSocket, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
-        	printf("\nError Connecting");
-    	else
-        	printf("\nSuccessfully Connected");
-   
-    	bzero(request, 1000);
- 
-    	sprintf(request, "Get %s HTTP/1.1\r\n Host: %s\r\n \r\n \r\n", request, host);
-     
-    	printf("\n%s", request);
-     
-    	if (send(tcpSocket, request, strlen(request), 0) < 0)
-        	printf("Error with send()");
-    	else
-        	printf("Successfully sent html fetch request");
-     
-    	bzero(request, 1000);
-     
-    	recv(tcpSocket, merged_request, 999, 0);
-    	printf("\n%s", merged_request);
-    	printf("\nhello");
-     
-    	close(tcpSocket);
-     
-    	return (1);
+ 
+    	bzero((char *) &serveraddr, sizeof(serveraddr));
+    	serveraddr.sin_family = AF_INET;
 
+	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+     
+    	serveraddr.sin_port = htons(port);
+	
+	if (connect(tcpSocket, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
+        	printf("\nError Connecting");
+    	else
+        	printf("\nSuccessfully Connected\n");
+ 
+    	sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/plain\r\n\r\n", resource_path, host);
+  	printf("\nCreated Get Request is below:\n\n\n");
+ 
+	printf("%s", request);
+	
+	if( send(tcpSocket , request , strlen(request) , 0) < 0)
+    	{
+        	printf("Send failed");
+        	return 1;
+    	}
+    	printf("Data Sent\n");
+
+	//Now receive full data
+    	readSocket(tcpSocket, input_buffer);
+    	return 0;
+  }
 }
+
+static int readSocket(int socket, char *buffer)
+{
+    ssize_t bytes_read = 0;
+	
+    //testing stuff, BUFSIZE = 4096
+    printf("Received Socket # %d\nBuffer Size = %d\n", socket, BUFSIZE);
+	
+    bytes_read = recv(socket, buffer, BUFSIZE - 1, 0);
+    while (bytes_read > 0)
+    {
+        buffer[bytes_read] = 0; // Null-terminate the buffer
+        printf("Buffer: %s\n", buffer);	
+        bytes_read = recv(socket, buffer, BUFSIZE - 1, 0);		
+    }
+    if (bytes_read == -1)
+    {
+        // Requires inclusion of <errno.h>
+        printf("Socket recv failed");
+        return -1;
+    }
+    return 0;
 }
+
