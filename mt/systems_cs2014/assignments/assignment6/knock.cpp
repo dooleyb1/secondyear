d4d9ab66f052e5c3c788000e699b50a723c7d2aa
Brandon Dooley <dooleyb1@tcd.ie>
2017-12-02 14:17:35 +0000

    GET Request writing correctly to file

--- mt/systems_cs2014/assignments/assignment6/knock.cpp
+++ mt/systems_cs2014/assignments/assignment6/knock.cpp
@@ -1,32 +1,25 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <string>
 #include <arpa/inet.h>
-#include <errno.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <unistd.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include<fcntl.h> 		
+#include <netdb.h>		
 #include <fstream>
-#include <sys/time.h>
 #include <iostream>
 
 using namespace std;
 #define BUFSIZE 4096
 
-
-/* Globals */
+//Globals
 bool hostflag, portflag, helpflag, webflag, fileflag;
-//Receiving function
+FILE * outputfile;
+const char* extension = ".txt";
+
+//Function for reading and writing data recievedfrom socket
 static int readSocket(int socket, char *buffer);
 
+//Usage function
 void usage(int n)
 {
   string usageString("usage: ./knock -h host -p port [-H] [-w] [-f file]");
@@ -37,9 +30,12 @@ void usage(int n)
 
 int main(int argc, char **argv)
 {
-	//Initialise flags to false
-	hostflag = portflag = helpflag = webflag = fileflag = false;
-
+    //Initialise all flags to false
+    hostflag = portflag = helpflag = webflag = fileflag = false;
+    
+    //Initialise variables
+    char filename[500];
+    char* name_with_extension;
     char host[500];
     char resource_path[500];
     char request[1000];
@@ -60,33 +56,29 @@ int main(int argc, char **argv)
 			switch(argv[i][j]) {
 			case 'h':
 			  hostflag = true;
-			  memcpy(host, argv[2], strlen(argv[2])+1);
- 			  cout << "hostflag set to true" << "\n";
-			  cout << "hostvalue entered = " << host << "\n";
+			  memcpy(host, argv[i+1], strlen(argv[i+1])+1);
 			  break;
 			case 'p':
 			  portflag = true;
-			  port = atoi(argv[4]);
-			  cout << "portflag set to true" << "\n";
-			  cout << "portvalue entered = " << port << "\n";
+			  port = atoi(argv[i+1]);
 			  break;
 			case 'H':
 			  helpflag = true;
-			  cout << "helpflag set to true" << "\n";
 			  break;
 			case 'w':
 			  webflag = true;
-			  memcpy(resource_path, argv[6], strlen(argv[6])+1);
-			  cout << "webflag set to true" << "\n";
-			  cout << "HTTP get request is : " << resource_path << "\n";	
+			  memcpy(resource_path, argv[i+1], strlen(argv[i+1])+1);	
 			  break;
 			case 'f':
 			  fileflag = true;
-			  cout << "fileflag set to true" << "\n";
+			  memcpy(filename, argv[i+1], strlen(argv[i+1])+1);
+		    	  name_with_extension = (char*) malloc(strlen(filename)+1+4);
+			  strcpy(name_with_extension, filename); 
+			  strcat(name_with_extension, extension);
+			  outputfile = fopen (name_with_extension,"w+");
 			  break;
 			case '?':
 			  helpflag = true;
-			  cout << "helpflag set to true" << "\n";
 			  break;
 			default:
 			  //If no flag has been set, then invalid input 
@@ -104,42 +96,39 @@ int main(int argc, char **argv)
 			for(j=0;argv[i][j];j++) {
 	    		if (!strcmp("host", argv[i])) {
 	     		  hostflag = true;
-			  memcpy(host, argv[2], strlen(argv[2])+1);
- 			  cout << "hostflag set to true" << "\n";
-			  cout << "hostvalue entered = " << host << "\n";
+			  memcpy(host, argv[i+1], strlen(argv[i+1])+1);
 	     		  break;
 	    		}
 	    		if (!strcmp("port",argv[i])) {
 	    		  portflag = true;
-			  port = atoi(argv[4]);
-			  cout << "portflag set to true" << "\n";
-			  cout << "portvalue entered = " << port << "\n";
+			  port = atoi(argv[i+1]);
 	     		  break;
 	    		}
-				if (!strcmp("help",argv[i])) {
+			if (!strcmp("help",argv[i])) {
 	    		  helpflag = true;
-			  cout << "helpflag set to true" << "\n";
 	     		  break;
 	    		}
-				if (!strcmp("web",argv[i])) {
-	    		 	 webflag = true;
-			 		 memcpy(resource_path, argv[6], strlen(argv[6])+1);
-			  		 cout << "webflag set to true" << "\n";
-			  		 cout << "HTTP get request is : " << resource_path << "\n";
-	     		     break;
+			if (!strcmp("web",argv[i])) {
+	    		  webflag = true;
+			  memcpy(resource_path, argv[i+1], strlen(argv[i+1])+1);
+	     		  break;
 	    		}
-				if (!strcmp("file",argv[i])) {
+			if (!strcmp("file",argv[i])) {
 	    		  fileflag = true;
-    			  cout << "fileflag set to true" << "\n";
+			  memcpy(filename, argv[i+1], strlen(argv[i+1])+1);
+		    	  name_with_extension = (char*) malloc(strlen(filename)+1+4);
+			  strcpy(name_with_extension, filename); 
+			  strcat(name_with_extension, extension);
+			  outputfile = fopen (name_with_extension,"w+");	
 	     		  break;
 	    		}
-				//If no flag has been set, then invalid input 
-			  	else if(!hostflag && !portflag && !helpflag && !webflag && !fileflag) {
-				  fprintf(stderr,"knock: Invalid argument -`%c'.\n",argv[i][j]);
-				  usage(1);
-				  exit(1);
-				  break;
-			  	}
+			//If no flag has been set, then invalid input 
+			else if(!hostflag && !portflag && !helpflag && !webflag && !fileflag) {
+			   fprintf(stderr,"knock: Invalid argument -`%s'.\n",argv[i]);
+			   usage(1);
+			   exit(1);
+			   break;
+			 }
 			}
 		}
 	}
@@ -149,57 +138,44 @@ int main(int argc, char **argv)
 	}
 	
 	else{
-		int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
+	//Create socket 
+	int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
      
     	if (tcpSocket < 0)
         	printf("\nError opening socket");
     	else
-        	printf("\nSuccessfully opened socket");
-     
+        
+	//Get IP Address using DNS
     	server = gethostbyname(host);
      
     	if (server == NULL)
     	{
         	printf("gethostbyname() failed\n");
     	}
-    	else
-    	{
-        	printf("\n%s = ", server->h_name);
-        	unsigned int j = 0;
-        	while (server -> h_addr_list[j] != NULL)
-        	{
-           		printf("%s", inet_ntoa(*(struct in_addr*)(server -> h_addr_list[j])));
-            		j++;
-        	}
-	}
-	printf("\n");
  
+	
     	bzero((char *) &serveraddr, sizeof(serveraddr));
     	serveraddr.sin_family = AF_INET;
-
 	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-     
     	serveraddr.sin_port = htons(port);
 	
+	//Connect to socket
 	if (connect(tcpSocket, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
         	printf("\nError Connecting");
-    	else
-        	printf("\nSuccessfully Connected\n");
  
-    	sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/plain\r\n\r\n", resource_path, host);
-  	printf("\nCreated Get Request is below:\n\n\n");
- 
-	printf("%s", request);
-	
+	//Format HTTP GET Request 
+    	sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s\r\nContent-Type: text/plain\r\n\r\n", 		resource_path, host);
+		
+	//Send HTTP GET Request
 	if( send(tcpSocket , request , strlen(request) , 0) < 0)
     	{
         	printf("Send failed");
         	return 1;
     	}
-    	printf("Data Sent\n");
 
-	//Now receive full data
+	//Receive data from socket and write to user defined file
     	readSocket(tcpSocket, input_buffer);
+	printf("Respone written to file %s...\n", name_with_extension);
     	return 0;
   }
 }
@@ -207,16 +183,24 @@ int main(int argc, char **argv)
 static int readSocket(int socket, char *buffer)
 {
     ssize_t bytes_read = 0;
-	
-    //testing stuff, BUFSIZE = 4096
-    printf("Received Socket # %d\nBuffer Size = %d\n", socket, BUFSIZE);
-	
     bytes_read = recv(socket, buffer, BUFSIZE - 1, 0);
+    int i=0;
     while (bytes_read > 0)
     {
-        buffer[bytes_read] = 0; // Null-terminate the buffer
-        printf("Buffer: %s\n", buffer);	
-        bytes_read = recv(socket, buffer, BUFSIZE - 1, 0);		
+	//Null terminate buffer
+        buffer[bytes_read] = 0; 
+	//If fileflag set, write to file 
+	if(fileflag)
+	{
+	    fprintf (outputfile, "%s", buffer);
+	    bytes_read = recv(socket, buffer, BUFSIZE - 1, 0);
+	}
+	//Otherwise print buffer to console
+	else
+	{
+            printf("%s", buffer);	
+            bytes_read = recv(socket, buffer, BUFSIZE - 1, 0);	
+	}	
     }
     if (bytes_read == -1)
     {
@@ -224,6 +208,7 @@ static int readSocket(int socket, char *buffer)
         printf("Socket recv failed");
         return -1;
     }
+    fclose (outputfile);
     return 0;
 }
 
