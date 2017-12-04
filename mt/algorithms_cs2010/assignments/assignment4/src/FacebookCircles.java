8e51981c3967f018d967ed287c309f70327564a9
Brandon Dooley <dooleyb1@tcd.ie>
2017-12-04 12:24:04 +0000

    added assignment4

---
+++ mt/algorithms_cs2010/assignments/assignment4/src/FacebookCircles.java
@@ -0,0 +1,62 @@
+/**
+ * Class FacebookCircles: calculates the statistics about the friendship circles in facebook data.
+ *
+ * @author
+ *
+ * @version 01/12/15 02:03:28
+ */
+public class FacebookCircles {
+
+  /**
+   * Constructor
+   * @param numberOfFacebookUsers : the number of users in the sample data.
+   * Each user will be represented with an integer id from 0 to numberOfFacebookUsers-1.
+   */
+  public FacebookCircles(int numberOfFacebookUsers) {
+    // TODO
+  }
+
+  /**
+   * creates a friendship connection between two users, represented by their corresponding integer ids.
+   * @param user1 : int id of first user
+   * @param user2 : int id of second  user
+   */
+  public void friends( int user1, int user2 ) {
+
+    // TODO
+  }
+  
+  /**
+   * @return the number of friend circles in the data already loaded.
+   */
+  public int numberOfCircles() {
+    // TODO
+    return 0;
+  }
+
+  /**
+   * @return the size of the largest circle in the data already loaded.
+   */
+  public int sizeOfLargestCircle() {
+    // TODO
+    return 0;
+  }
+
+  /**
+   * @return the size of the median circle in the data already loaded.
+   */
+  public int sizeOfAverageCircle() {
+    // TODO
+    return 0;
+  }
+
+  /**
+   * @return the size of the smallest circle in the data already loaded.
+   */
+  public int sizeOfSmallestCircle() {
+    // TODO
+    return 0;
+  }
+
+
+}
