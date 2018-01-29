b5aa608b226e8cd0440581d21c814fc39d8a4da4
Brandon Dooley <dooleyb1@tcd.ie>
2018-01-29 09:06:46 +0000

    adding week2 files

---
+++ ht/algorithms_and_data_structures_cs2010/assignments/assignment1/SortComparison.java
@@ -0,0 +1,97 @@
+// -------------------------------------------------------------------------
+
+/**
+ *  This class contains static methods that implementing sorting of an array of numbers
+ *  using different sort algorithms.
+ *
+ *  @author
+ *  @version HT 2018
+ */
+
+ class SortComparison {
+
+    /**
+     * Sorts an array of doubles using InsertionSort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order.
+     *
+     */
+    static double [] insertionSort (double a[]){
+
+        //todo: implement the sort
+    }//end insertionsort
+
+    /**
+     * Sorts an array of doubles using Quick Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] quickSort (double a[]){
+	
+		 //todo: implement the sort
+
+    }//end quicksort
+
+    /**
+     * Sorts an array of doubles using Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] mergeSort (double a[]){
+
+		 //todo: implement the sort
+	
+    }//end mergesort
+
+    /**
+     * Sorts an array of doubles using Shell Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] shellSort (double a[]){
+
+		 //todo: implement the sort
+		 
+    }//end shellsort
+
+    /**
+     * Sorts an array of doubles using Selection Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] selectionSort (double a[]){
+
+         //todo: implement the sort
+
+    }//end selectionsort
+
+    /**
+     * Sorts an array of doubles using Bubble Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] bubbleSort (double a[]){
+
+         //todo: implement the sort
+		 
+    }//end bubblesort
+
+
+    public static void main(String[] args) {
+
+        //todo: do experiments as per assignment instructions
+    }
+
+ }//end class
+
