aab42264e13d46b51ca39200e0a127339d39cbc5
Brandon Dooley <dooleyb1@tcd.ie>
2018-01-31 09:44:20 +0000

    sort algos implemented, testing needed

--- ht/algorithms_and_data_structures_cs2010/assignments/assignment1/SortComparison.java
+++ ht/algorithms_and_data_structures_cs2010/assignments/assignment1/SortComparison.java
@@ -21,74 +21,89 @@
 
         int n = a.length;
 
-	//Iterate through input array
-	for(int i=0; i<n; i++)
-	{		
-		int temp = a[i];
-		//Compare a[i] with a[i-1]
-		int j = i-1;
-		
-		//Iterate through all possible comparisons and swap accordingly	
-		while(j>=0 && a[j] < temp)
-		{
-			a[j+1] = a[j]
-			j = j-1;
+		//Iterate through input array
+		for(int i=0; i<n; i++)
+		{		
+			double temp = a[i];
+			//Compare a[i] with a[i-1]
+			int j = i-1;
+			
+			//Iterate through all possible comparisons and swap accordingly	
+			while(j>=0 && a[j] < temp)
+			{
+				a[j+1] = a[j];
+				j = j-1;
+			}
+			a[j+1] = temp;
 		}
-		a[j+1] = temp;
 		return a;
     }
 
     /**
      * Sorts an array of doubles using Quick Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
+     * Method implemented using https://www.programcreek.com/2012/11/quicksort-array-in-java/
      * @param a: An unsorted array of doubles.
      * @return array sorted in ascending order
      *
      */
     static double [] quickSort (double a[]){
 	
-		sort(a, 0, a.length-1);
+    	int low = 0;
+    	int high = a.length-1;
+    	
+    	qSort(a, low, high);
+    	return a;
+    }
+    
+    /**
+     * This method implements quickSort recursively for a given array of doubles.
+     * @param a: Unsorted array of doubles.
+     * @param low: Low index.
+     * @param high: High index.
+     */
+    private static void qSort(double a[], int low, int high) {
+    	
+    	if (a == null || a.length == 0)
+			return;
+ 
+		if (low >= high)
+			return;
+ 
+		//Find pivot value
+		int middle = low + (high - low) / 2;
+		double pivot = a[middle];
 		
-		private static void sort(double a[], int lo, int hi){
-
-			if(hi <= lo)
-				return;
-
-			int j = partition(a, low, high);
-
-			//Sort left partition
-			sort(a, low, j-1);
-
-			//Sort right partition
-			sort(a, j+1, high);
+		//Sort values < pivot to left and > pivot to right
+		int i = low;
+		int j = high;
 		
-		}
-	
-		private static int partition(double a[], int lo, int hi){
-
-			int pivot = arr[hi];
-			int i = (lo-1);
-
-			for(int j=lo; j<hi; j++)
-			{
-				if(a[j] <= pivot)
-				{
-					i++;
-					
-					//Swap a[i] and a[j]
-					int temp = a[i];
-					a[i] = a[j];
-					a[j] = temp;
-				}
+		while(i<=j) {
+			//Find low value !< pivot
+			while (a[i] < pivot) {
+				i++;
 			}
-
-			int temp = a[i+1];
-			a[i+1] = a[hi];
-			a[hi] = temp;
+			//Find high value !> pivot
+			while(a[j] > pivot) {
+				j--;
+			}
 			
-			return i+1;
+			if(i <= j) {
+				double tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+				i++;
+				j--;
+			}
 		}
-
+		
+		//Recursively quicksort two sub parts
+		if(low < j)
+			qSort(a, low, j);
+		
+		if(high > i)
+			qSort(a, i, high);
+    	
     }
 
     /**
@@ -100,48 +115,186 @@
      */
     static double [] mergeSort (double a[]){
 
-		 //todo: implement the sort
+		 int low = 0;
+		 int high = a.length - 1;
+		 
+		 double tmp[] = new double[a.length];
+		 
+		 mSort(a, tmp, low, high);
+		 return a;
 	
-    }//end mergesort
+    }
+    
+    /**
+     * Performs mergesort recursively within the given indices low and high
+     * @param a: An unsorted array of doubles
+     * @param tmp: Temp array to be used for the mergesort.
+     * @param low: Lower index
+     * @param high: Higher index
+     */
+    private static void mSort(double a[], double tmp[], int low, int high) {
+    	
+    	//If low is smaller than high array is unsorted
+		if(low < high) {
+			//Get index of middle element
+			int middle = low + (high - low) / 2;
+			//Sort left array
+			mSort(a, tmp, low, middle);
+			//Sort right array
+			mSort(a, tmp, middle + 1, high);
+			//Combine them both
+			merge(a, tmp, low, middle, high);
+			
+		 }
+    	
+    }
+    
+    /**
+     * Combines recursive mergesorts into the one larger array
+     * Algorithm implemented using http://www.vogella.com/tutorials/JavaAlgorithmsMergesort/article.html
+     * @param a: An unsorted array of doubles.
+     * @param tmp: Temp array to be used in the mergesort.
+     * @param low: Index of lower element.
+     * @param middle: Index of middle element.
+     * @param high: Index of higher element.
+     */
+    private static void merge(double a[], double tmp[], int low, int middle, int high) {
+    	
+    	 // Copy both parts into the tmp array
+        for (int i = low; i <= high; i++) {
+            tmp[i] = a[i];
+        }
+        
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        
+    	// Copy the smallest values from either the left or the right side back
+        // to the original array
+        while (i <= middle && j <= high) {
+            if (tmp[i] <= tmp[j]) {
+                a[k] = a[i];
+                i++;
+            } else {
+               a[k] = tmp[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy the rest of the left side of the array into the target array
+        while (i <= middle) {
+            a[k] = tmp[i];
+            k++;
+            i++;
+        }
+        // Since we are sorting in-place any leftover elements from the right side
+        // are already at the right position.
+    	
+    }
 
     /**
      * Sorts an array of doubles using Shell Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
+     * Implemented using https://www.geeksforgeeks.org/shellsort/
      * @param a: An unsorted array of doubles.
      * @return array sorted in ascending order
      *
      */
     static double [] shellSort (double a[]){
 
-		 //todo: implement the sort
+		 int n = a.length;
 		 
-    }//end shellsort
+		 //Start with a gap of a.length/2, keep reducing by half
+		 for(int gap = n/2; gap > 0; gap /= 2) {
+			 
+			 //Perform insertion sort within the gaps 
+			 for(int i = gap; i < n; i += 1) {
+				
+				 //Store a[i] into tmp
+				 double tmp = a[i];
+				 
+				 //Shift earlier gap-sorted elements up until a[i] position is found
+				 int j;
+				 for(j=i; j>=gap && a[j-gap] > tmp; j -= gap) {
+					 
+					 a[j] = a[j-gap];
+					 
+				 }
+				 
+				 //Place tmp in its correct location
+				 a[j] = tmp;
+			 }
+		 }
+		 return a;
+    }
 
     /**
      * Sorts an array of doubles using Selection Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
+     * Implemented using https://www.geeksforgeeks.org/selection-sort/
      * @param a: An unsorted array of doubles.
      * @return array sorted in ascending order
      *
      */
     static double [] selectionSort (double a[]){
 
-         //todo: implement the sort
+         int n = a.length;
+         double tmp;
+         //Increment the unsorted array index by one
+         for(int i = 0; i < n-1; i++) {
+        	 //Find index of min element 
+        	 int min = i;
+        	 for(int j = i+1; j < n; j++) {
+        		 if(a[j] < a[min])
+        			 min = j;
+        	 }
+        	 
+        	 //Swap the found min with the element a[i]
+        	 tmp = a[min];
+        	 a[min] = a[i];
+        	 a[i] = tmp;
+         }
+         return a;
 
-    }//end selectionsort
+    }
 
     /**
      * Sorts an array of doubles using Bubble Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
+     * Implemented using https://www.geeksforgeeks.org/bubble-sort/
      * @param a: An unsorted array of doubles.
      * @return array sorted in ascending order
      *
      */
     static double [] bubbleSort (double a[]){
 
-         //todo: implement the sort
+         int n = a.length;
+         int i, j;
+         double tmp;
+         boolean swapped;
+         
+         //Increment over entire array
+         for(i = 0; i < n; i++) {
+        	 
+        	 swapped = false;
+        	 for(j = 0; j < n-i-1; j++) {
+        		 
+        		 //Swap a[j] with a[j+1] if larger
+        		 if(a[j] > a[j+1]) {
+        			 tmp = a[j];
+        			 a[j] = a[j+1];
+        			 a[j+1] = tmp;
+        			 swapped = true;
+        		 }
+        	 }
+        	 
+        	 //If no elements swapped during inner loop, break
+        	 if(!swapped)
+        		 break;
+         }
+         return a;
 		 
-    }//end bubblesort
+    }
 
 
     public static void main(String[] args) {
