647c2c474ce5ed3c7c9bf957a90bf7a331b4592f
Brandon Dooley <dooleyb1@tcd.ie>
2018-02-01 09:28:18 +0000

    first round testing added

--- ht/algorithms_and_data_structures_cs2010/assignments/assignment1/SortComparisonTest.java
+++ ht/algorithms_and_data_structures_cs2010/assignments/assignment1/SortComparisonTest.java
@@ -79,21 +79,6 @@ public class SortComparisonTest
     	sortedArray = SortComparison.insertionSort(a);
     	assertArrayEquals("Checking insertionSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
     	
-    	sortedArray = SortComparison.quickSort(a);
-    	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
-    	
-    	sortedArray = SortComparison.mergeSort(a);
-    	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
-    	
-    	sortedArray = SortComparison.shellSort(a);
-    	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
-    	
-    	sortedArray = SortComparison.selectionSort(a);
-    	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
-    	
-    	sortedArray = SortComparison.bubbleSort(a);
-    	assertArrayEquals("Checking bubbleSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
-    	
     	//Testing on an array of one element
     	double b[] = {104.0};
     	double expectedArray2[] = {104.0}; 
@@ -101,21 +86,6 @@ public class SortComparisonTest
     	
     	sortedArray2 = SortComparison.insertionSort(b);
     	assertArrayEquals("Checking insertionSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
-    	
-    	sortedArray2 = SortComparison.quickSort(b);
-    	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
-    	
-    	sortedArray2 = SortComparison.mergeSort(b);
-    	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
-    	
-    	sortedArray2 = SortComparison.shellSort(b);
-    	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
-    	
-    	sortedArray2 = SortComparison.selectionSort(b);
-    	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
-    	
-    	sortedArray2 = SortComparison.bubbleSort(b);
-    	assertArrayEquals("Checking bubbleSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
     	
     	//Testing on an array of two elements
     	double c[] = {104.0, -1230.293};
@@ -125,17 +95,209 @@ public class SortComparisonTest
     	sortedArray3 = SortComparison.insertionSort(c);
     	assertArrayEquals("Checking insertionSort on a test array, returned array should equal expected array", expectedArray3, sortedArray3, 0);
     	
+    	//Testing on an array of three elements
+    	double d[] = {104.0, -1230.293, 37261.348};
+    	double expectedArray4[] = {-1230.293, 104.0, 37261.348}; 
+    	double sortedArray4[] = new double[d.length];
+    	
+    	sortedArray4 = SortComparison.insertionSort(d);
+    	assertArrayEquals("Checking insertionSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
+    	
+    }
+
+
+ // ----------------------------------------------------------
+    /**
+     * Check that the quickSort() method works properly
+     */
+    @Test
+    public void testQuickSort()
+    {
+    	//Testing on a large array of random order and numbers
+    	double a[] = {21.3, 5.6, 1.2, 90.5, 10.1, 60.4, 99.0, 0.2, -1.0, 2.1, 1293.2, 6.3};
+    	double expectedArray[] = {-1.0, 0.2, 1.2, 2.1, 5.6, 6.3, 10.1, 21.3, 60.4, 90.5, 99.0, 1293.2}; 
+    	double sortedArray[] = new double[a.length];
+    	
+    	sortedArray = SortComparison.quickSort(a);
+    	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
+    	
+    	//Testing on an array of one element
+    	double b[] = {104.0};
+    	double expectedArray2[] = {104.0}; 
+    	double sortedArray2[] = new double[b.length];
+    	
+
+    	sortedArray2 = SortComparison.quickSort(b);
+    	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
+    	
+    	//Testing on an array of two elements
+    	double c[] = {104.0, -1230.293};
+    	double expectedArray3[] = {-1230.293, 104.0}; 
+    	double sortedArray3[] = new double[c.length];	
+
     	sortedArray3 = SortComparison.quickSort(c);
     	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray3, sortedArray3, 0);
+    	
+    	//Testing on an array of three elements
+    	double d[] = {104.0, -1230.293, 37261.348};
+    	double expectedArray4[] = {-1230.293, 104.0, 37261.348}; 
+    	double sortedArray4[] = new double[d.length];
+    	
+    	sortedArray4 = SortComparison.quickSort(d);
+    	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
+    	
+    }
+    
+	// ----------------------------------------------------------
+    /**
+     * Check that the mergeSort() method works properly
+     */
+    @Test
+    public void testMergeSort()
+    {
+    	//Testing on a large array of random order and numbers
+    	double a[] = {21.3, 5.6, 1.2, 90.5, 10.1, 60.4, 99.0, 0.2, -1.0, 2.1, 1293.2, 6.3};
+    	double expectedArray[] = {-1.0, 0.2, 1.2, 2.1, 5.6, 6.3, 10.1, 21.3, 60.4, 90.5, 99.0, 1293.2}; 
+    	double sortedArray[] = new double[a.length];
+    	
+    	sortedArray = SortComparison.mergeSort(a);
+    	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
+    	
+    	//Testing on an array of one element
+    	double b[] = {104.0};
+    	double expectedArray2[] = {104.0}; 
+    	double sortedArray2[] = new double[b.length];
+    	
+    	sortedArray2 = SortComparison.mergeSort(b);
+    	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
+    	
+    	//Testing on an array of two elements
+    	double c[] = {104.0, -1230.293};
+    	double expectedArray3[] = {-1230.293, 104.0}; 
+    	double sortedArray3[] = new double[c.length];
     	
     	sortedArray3 = SortComparison.mergeSort(c);
     	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray3, sortedArray3, 0);
     	
+    	//Testing on an array of three elements
+    	double d[] = {104.0, -1230.293, 37261.348};
+    	double expectedArray4[] = {-1230.293, 104.0, 37261.348}; 
+    	double sortedArray4[] = new double[d.length];
+    	
+    	sortedArray4 = SortComparison.mergeSort(d);
+    	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
+    	
+    }
+    
+ // ----------------------------------------------------------
+    /**
+     * Check that the shellSort() method works properly
+     */
+    @Test
+    public void testShellSort()
+    {
+    	//Testing on a large array of random order and numbers
+    	double a[] = {21.3, 5.6, 1.2, 90.5, 10.1, 60.4, 99.0, 0.2, -1.0, 2.1, 1293.2, 6.3};
+    	double expectedArray[] = {-1.0, 0.2, 1.2, 2.1, 5.6, 6.3, 10.1, 21.3, 60.4, 90.5, 99.0, 1293.2}; 
+    	double sortedArray[] = new double[a.length];
+    	
+    	sortedArray = SortComparison.shellSort(a);
+    	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
+    	
+    	//Testing on an array of one element
+    	double b[] = {104.0};
+    	double expectedArray2[] = {104.0}; 
+    	double sortedArray2[] = new double[b.length];
+    	
+    	sortedArray2 = SortComparison.shellSort(b);
+    	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
+    	
+    	//Testing on an array of two elements
+    	double c[] = {104.0, -1230.293};
+    	double expectedArray3[] = {-1230.293, 104.0}; 
+    	double sortedArray3[] = new double[c.length];
+    	
     	sortedArray3 = SortComparison.shellSort(c);
     	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray3, sortedArray3, 0);
     	
+    	//Testing on an array of three elements
+    	double d[] = {104.0, -1230.293, 37261.348};
+    	double expectedArray4[] = {-1230.293, 104.0, 37261.348}; 
+    	double sortedArray4[] = new double[d.length];
+    	
+    	sortedArray4 = SortComparison.selectionSort(d);
+    	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
+    	
+    }
+    
+ // ----------------------------------------------------------
+    /**
+     * Check that the selectionSort() method works properly
+     */
+    @Test
+    public void testSelectionSort()
+    {
+    	//Testing on a large array of random order and numbers
+    	double a[] = {21.3, 5.6, 1.2, 90.5, 10.1, 60.4, 99.0, 0.2, -1.0, 2.1, 1293.2, 6.3};
+    	double expectedArray[] = {-1.0, 0.2, 1.2, 2.1, 5.6, 6.3, 10.1, 21.3, 60.4, 90.5, 99.0, 1293.2}; 
+    	double sortedArray[] = new double[a.length];
+    	
+    	sortedArray = SortComparison.selectionSort(a);
+    	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
+    	
+    	//Testing on an array of one element
+    	double b[] = {104.0};
+    	double expectedArray2[] = {104.0}; 
+    	double sortedArray2[] = new double[b.length];
+    	
+    	sortedArray2 = SortComparison.selectionSort(b);
+    	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
+    	
+    	//Testing on an array of two elements
+    	double c[] = {104.0, -1230.293};
+    	double expectedArray3[] = {-1230.293, 104.0}; 
+    	double sortedArray3[] = new double[c.length];
+    	
     	sortedArray3 = SortComparison.selectionSort(c);
     	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray3, sortedArray3, 0);
+    	
+    	//Testing on an array of three elements
+    	double d[] = {104.0, -1230.293, 37261.348};
+    	double expectedArray4[] = {-1230.293, 104.0, 37261.348}; 
+    	double sortedArray4[] = new double[d.length];
+    	
+    	sortedArray4 = SortComparison.shellSort(d);
+    	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
+    	
+    }
+    
+ // ----------------------------------------------------------
+    /**
+     * Check that the bubbleSort() method works properly
+     */
+    @Test
+    public void testBubbleSort()
+    {
+    	//Testing on a large array of random order and numbers
+    	double a[] = {21.3, 5.6, 1.2, 90.5, 10.1, 60.4, 99.0, 0.2, -1.0, 2.1, 1293.2, 6.3};
+    	double expectedArray[] = {-1.0, 0.2, 1.2, 2.1, 5.6, 6.3, 10.1, 21.3, 60.4, 90.5, 99.0, 1293.2}; 
+    	double sortedArray[] = new double[a.length];
+    	
+    	sortedArray = SortComparison.bubbleSort(a);
+    	assertArrayEquals("Checking bubbleSort on a test array, returned array should equal expected array", expectedArray, sortedArray, 0);
+    	
+    	//Testing on an array of one element
+    	double b[] = {104.0};
+    	double expectedArray2[] = {104.0}; 
+    	double sortedArray2[] = new double[b.length];
+    	
+    	sortedArray2 = SortComparison.bubbleSort(b);
+    	assertArrayEquals("Checking bubbleSort on a test array, returned array should equal expected array", expectedArray2, sortedArray2, 0);
+    	
+    	//Testing on an array of two elements
+    	double c[] = {104.0, -1230.293};
+    	double expectedArray3[] = {-1230.293, 104.0}; 
+    	double sortedArray3[] = new double[c.length];
     	
     	sortedArray3 = SortComparison.bubbleSort(c);
     	assertArrayEquals("Checking bubbleSort on a test array, returned array should equal expected array", expectedArray3, sortedArray3, 0);
@@ -145,29 +307,10 @@ public class SortComparisonTest
     	double expectedArray4[] = {-1230.293, 104.0, 37261.348}; 
     	double sortedArray4[] = new double[d.length];
     	
-    	sortedArray4 = SortComparison.insertionSort(d);
-    	assertArrayEquals("Checking insertionSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
-    	
-    	sortedArray4 = SortComparison.quickSort(d);
-    	assertArrayEquals("Checking quickSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
-    	
-    	sortedArray4 = SortComparison.mergeSort(d);
-    	assertArrayEquals("Checking mergeSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
-    	
-    	sortedArray4 = SortComparison.shellSort(d);
-    	assertArrayEquals("Checking shellSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
-    	
-    	sortedArray4 = SortComparison.selectionSort(d);
-    	assertArrayEquals("Checking selectionSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
-    	
     	sortedArray4 = SortComparison.bubbleSort(d);
     	assertArrayEquals("Checking bubbleSort on a test array, returned array should equal expected array", expectedArray4, sortedArray4, 0);
     	
     }
-
-
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
-    // be executed at least once from at least one test.
 
     // ----------------------------------------------------------
     /**
