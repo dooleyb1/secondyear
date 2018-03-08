5c711fd85149740c6c7862b439ad34d8b22c0273
Brandon Dooley <dooleyb1@tcd.ie>
2018-03-08 16:44:33 +0000

    formatting

--- ht/computer_architecture_cs2022/labs/assignment1/testbenches/mux2_16bit_tb.vhd
+++ ht/computer_architecture_cs2022/labs/assignment1/testbenches/mux2_16bit_tb.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/14/2018 12:22:16 PM
--- Design Name: 
--- Module Name: mux2_16bit_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity mux2_16bit_tb is
 --  Port ( );
@@ -37,33 +7,39 @@ end mux2_16bit_tb;
 
 architecture Behavioral of mux2_16bit_tb is
     -- declare component to test
+    
     component mux2_16bit is
-        Port ( In0 : in std_logic_vector(15 downto 0);
+        Port ( 
+        	In0 : in std_logic_vector(15 downto 0);
             In1 : in std_logic_vector(15 downto 0);
             s : in std_logic;
-            Z : out std_logic_vector(15 downto 0));
+            Z : out std_logic_vector(15 downto 0;
+        );
     end component;
     
     -- signals for tests (initialise to 0)
-        
-        signal In0 : std_logic_vector(15 downto 0):= x"0000";
-        signal In1 : std_logic_vector(15 downto 0):= x"0000";
-        signal s : std_logic := '0';
-        signal Z : std_logic_vector(15 downto 0):= x"0000";
+       
+    --inputs    
+    signal In0 : std_logic_vector(15 downto 0):= x"0000";
+    signal In1 : std_logic_vector(15 downto 0):= x"0000";
+    signal s : std_logic := '0';
+    
+    --outputs
+    signal Z : std_logic_vector(15 downto 0):= x"0000";
         
 begin
 
     -- instantiate component for test, connect ports to internal signals
     UUT: mux2_16bit
-    Port map(
+    Port Map(
         In0 => In0,
         In1 => In1,
         s => s,
         Z => Z
     );
     
-    simulation_process :process
-    begin
+simulation_process :process
+begin
         
         In0 <= x"00FF";
         In1 <= x"00AA";
