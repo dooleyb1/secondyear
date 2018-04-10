b2c87ad5c8d69c39a4fbec57ccb1f1bdcb5e56aa
Brandon Dooley <dooleyb1@tcd.ie>
2018-04-10 17:18:01 +0100

    adding licences

--- mt/algorithms_and_data_structures_cs2010/assignments/assignment2/src/DoublyLinkedList.java
+++ mt/algorithms_and_data_structures_cs2010/assignments/assignment2/src/DoublyLinkedList.java
@@ -2,12 +2,28 @@ import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
 
-// -------------------------------------------------------------------------
-/**
- *  This class contains the methods of Doubly Linked List.
+/* MIT License
  *
- *  @author  Brandon Dooley (#16327446)
- *  @version 13/10/16 18:15
+ * Copyright (c) 2017 Brandon Dooley - dooleyb1@tcd.ie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
 
