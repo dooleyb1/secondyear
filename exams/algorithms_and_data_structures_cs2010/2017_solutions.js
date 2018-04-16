3d67a61cf27b86bc3bcb46110772c0067e60e2aa
Brandon Dooley <dooleyb1@tcd.ie>
2018-04-16 14:18:08 +0100

    algos study

--- exams/algorithms_and_data_structures_cs2010/2017_solutions.js
+++ exams/algorithms_and_data_structures_cs2010/2017_solutions.js
@@ -212,3 +212,172 @@
             return true;;
 
       }
+
+--------------------------------------------------------------------------
+** Question 3 ** - Shortest Paths
+--------------------------------------------------------------------------
+
+/*  a) Describe what is the postorder and the topological sort of the
+ *    vertices of a Directed Acyclic Graph
+ */
+
+  **Postorder** - A tree traversal where the left and right subtrees are
+                  recursively traversed before the root is finally
+                  traversed.
+
+  **Topological Sort** - Topological sort is when all of the vertices of
+                          a DAG point upwards. This is achieved by returning
+                          vertices in reverse postorder.
+
+/*  b) Explain in English the algorithm that finds the topological sort of
+ *     a DAG. You can presume DFS and BFS are known.
+ */
+
+   1) Run DFS on a graph and return all vertices in postorder.
+   2) Reverse all vertices to reversed postorder.
+
+/*  c) Give the postoder and topological sort of the vertices of the following
+ *      graph.
+*/
+
+  1) Postoder:
+
+      G F D E B C A
+
+  2) Topological Sort:
+
+      A C B E D F G
+
+/*  d) Give the postoder and topological sort of the vertices of the following
+ *      graph.
+*/
+
+    1) Dijkstras algorithm:
+
+      - Runtime = O(V^2) OR O(E + V log V) with a Fibonacci Heap
+      - Limitations = Non-Negative Weights, No Cycles
+      - Suited Graphs = Graphs with non-negative edgeweights
+
+    2) Topological Sort algorithm:
+
+      - Runtime = O(E + V)
+      - Limitations = No Directed Cycles
+      - Suited Graphs = Acyclic Edge-Weighted Digraphs
+
+    3) Bellman-Ford algorithm:
+
+      - Runtime = O(E * V)
+      - Limitations = No Negative Cycles
+      - Suited Graphs = Graphs with No Negative Cycles
+
+/*  e) Give the SPT, rooted at vertex A, in the above graph G. Which Algorithm
+ *      is best suited for finding the shortest paths in this graph
+*/
+
+    Topological Sort:
+
+        A C B E D F G
+
+
+    v    distTo[]    edgeTo[]
+    ---------------------------
+    A       0           -
+    B       10         A->B
+    C       10         A->C
+    D       20         B->D
+    E        5         B->E
+    F        0         E->F
+    G        5         F->G
+
+    -Best method suited for finding shortest path in this graph would be the
+      Topological Sort Algorithm as it is suited to Acyclic Edge-Weighted
+      Digraphs and allows for negative edge weightings.
+
+------------------------------------------------------------------------------
+** Question 3 ** - Shortest Paths
+------------------------------------------------------------------------------
+
+/*  a) Give the SPT, rooted at vertex A, in the above graph G. Which Algorithm
+ *      is best suited for finding the shortest paths in this graph
+*/
+
+public static int binarySearch(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+
+            if(key < a[mid])
+              hi = mid - 1;
+
+            else if(key > a[mid])
+              lo = mid + 1;
+
+            else
+              return mid;
+        }
+        return -1;
+    }
+
+/*  a) Consider the Basic_Matrix class on the next page. Implement from the
+*     Basic Matrix class the following Java method.
+*/
+
+    int min_index(int[] arr){
+
+      int minElement = arr[0];
+      int minIndex = 0;
+
+      //Since no sorting, have to iterate through array
+      for(int i=0 < i<arr.length(); i++){
+        if(arr[i] < minElement){
+          minElement = arr[i];
+          minIndex = i;
+        }
+      }
+    }
+
+/*  b) A Matrix M has a Saddle Point iff for some position (i,j), M(i,j) is the
+ *    minimum of row i and maximum of column j.
+*/
+
+    void one_saddle(Basic_Matrix m){
+
+      int rowIndex = 0;
+      int minRowVal = m[0][0];
+
+      int columnIndex = 0;
+      int maxColumnVal = m[0][0];
+
+      //Iterate over all rows
+      for(int i=0 < i<m.length(); i++){
+
+        minRowVal = m[i][0];
+        columnIndex = 0;
+
+        //Find the minimum element for each row
+        for(int j=1; j<m.length(); i++){
+          if(minRowVal > m[i][j]){
+            minRowVal = m[i][j];
+            minRowIndex = i;
+            columnIndex = j;
+          }
+        }
+
+        //Check if this mininmum element is also the max for its column;
+        int k;
+        for(k=0; k<n; k++){
+          if(minRowVal < mat[k][columnIndex])
+            break;
+        }
+
+        if(k == m.length()){
+          System.out.println("Value of saddle point is "
+                    + m[minRowIndex][minColumnIndex]);
+          return 0;
+        }
+      }
+      return 0;
+    }
